8d2cde024c4d654f49d9f20fe106f07f5779daa1
Joseph Charamut <12399761+joecharamut@users.noreply.github.com>
2022-06-02 13:13:35 -0400

    I THINK IT WORKS?

--- lib/new_darc.py
+++ lib/new_darc.py
@@ -1,8 +1,13 @@
 import io
 import os
-import typing
 import struct
-from typing import NamedTuple, List, Optional
+from typing import NamedTuple, List, Optional, Generator, Any
+from collections import namedtuple
+
+import hexdump
+
+# darc format reference:
+#   http://web.archive.org/web/20211123124701/http://problemkaputt.de/gbatek-3ds-files-archive-darc.htm
 
 
 class ByteOrder:
@@ -10,6 +15,12 @@ class ByteOrder:
         struct: str
         wchar: str
         bom: bytes
+
+        def pack(self, fmt: str, *args: Any) -> bytes:
+            return struct.pack(self.struct + fmt, *args)
+
+        def unpack(self, fmt: str, buffer: bytes) -> tuple:
+            return struct.unpack(self.struct + fmt, buffer)
 
     LITTLE_ENDIAN = Order("<", "utf-16-le", b"\xFF\xFE")
     BIG_ENDIAN = Order(">", "utf-16-be", b"\xFE\xFF")
@@ -31,7 +42,7 @@ class DarcEntry:
         self._parent = None
 
     def __repr__(self) -> str:
-        return f"<DarcEntry {'directory' if self._is_dir else 'file'} length={self.length}>"
+        return f"<DarcEntry '{self.name}' {'directory' if self._is_dir else 'file'} length={self.length}>"
 
     def dump(self, depth=0) -> None:
         print(f"{'-'*depth}| {self.name}")
@@ -63,7 +74,6 @@ class DarcEntry:
     def add_child(self, child: "DarcEntry") -> None:
         if not self._is_dir:
             raise TypeError("Cannot add children of a file")
-        print(f"add child: {self.name}->{child.name}")
         child._parent = self
         self._children.append(child)
 
@@ -76,12 +86,63 @@ class DarcEntry:
     @property
     def filename(self) -> str:
         stack = []
-        node = self
+        node: Optional[DarcEntry] = self
         while node is not None:
             stack.append(node.name)
             node = node._parent
         stack.reverse()
         return "/".join(stack)
+
+    def flat_tree(self) -> Generator["DarcEntry", None, None]:
+        yield self
+        if self._is_dir:
+            for node in self._children:
+                yield from node.flat_tree()
+
+    def breadth_tree(self) -> Generator["DarcEntry", None, None]:
+        if self._is_dir:
+            for node in self._children:
+                if node._is_dir:
+                    yield from node.breadth_tree()
+            for node in self._children:
+                if not node._is_dir:
+                    yield from node.breadth_tree()
+        yield self
+
+    def dir_entry_size(self) -> int:
+        i = 1
+        if self._is_dir:
+            for node in self._children:
+                i += node.dir_entry_size()
+        return i
+
+    @property
+    def is_dir(self) -> bool:
+        return self._is_dir
+
+    @property
+    def parent(self) -> Optional["DarcEntry"]:
+        return self._parent
+
+    @property
+    def children(self) -> List["DarcEntry"]:
+        if not self._is_dir:
+            raise TypeError("Files cannot have children")
+        return self._children
+
+    def __eq__(self, other) -> bool:
+        if self is other:
+            return True
+
+        if not isinstance(other, DarcEntry):
+            return False
+
+        return \
+            self._is_dir == other._is_dir and \
+            self.name == other.name and \
+            self._data == other._data and \
+            self._parent == other._parent and \
+            self._children == other._children
 
 
 class Darc:
@@ -108,11 +169,85 @@ class Darc:
             Darc.HEADER_STRUCT.format(self.byte_order.struct),
             0x1C,               # header length
             Darc.DARC_VERSION,  # version
-            0,                  # file length
+            0xFFFFFFFF,         # file length
             0x1C,               # file table offset
-            0,                  # file table length
-            0,                  # file data offset
+            0xFFFFFFFF,         # file table length
+            0xFFFFFFFF,         # file data offset
         ))
+
+        name_table = io.BytesIO()
+        filename_to_index = {None: 0, self.root_entry.filename: 0}
+        i = 0
+        for e in self.root_entry.flat_tree():
+            name_offset = name_table.tell()
+            name_table.write(e.name.encode(self.byte_order.wchar))
+            name_table.write(b"\x00\x00")
+
+            buf.write(struct.pack(
+                Darc.FILE_TABLE_ENTRY.format(self.byte_order.struct),
+                name_offset | (0x01000000 if e.is_dir else 0),
+                filename_to_index[e.parent.filename if e.parent else None] if e.is_dir else 0,
+                i + e.dir_entry_size() if e.is_dir else e.length,
+            ))
+
+            filename_to_index[e.filename] = i
+
+            i += 1
+
+        # print(filename_to_index)
+
+        # write out name table at end of file headers
+        buf.write(name_table.getvalue())
+
+        name_table_end = buf.tell()
+        file_table_length = name_table_end - 0x1C
+        buf.seek(0x14, os.SEEK_SET)
+        buf.write(struct.pack(f"{self.byte_order.struct} I", file_table_length))
+        buf.seek(name_table_end, os.SEEK_SET)
+
+        def align(b: io.BytesIO):
+            pos = b.tell()
+            extra = pos % 0x20
+            if extra > 0:
+                b.write(b"\x00"*(0x20 - extra))
+
+        align(buf)
+        file_data_start = buf.tell()
+        buf.seek(0x18, os.SEEK_SET)
+        buf.write(self.byte_order.pack("I", file_data_start))
+        buf.seek(file_data_start, os.SEEK_SET)
+
+        for e in self.root_entry.breadth_tree():
+            if e.is_dir:
+                continue
+
+            # print(f"writing file {e.filename} (idx {filename_to_index[e.filename]})")
+            # print(f"{hex(buf.tell())} -> {hexdump.hexdump(e.data[0:16], 'return')}")
+
+            align(buf)
+            file_pos = buf.tell()
+            buf.write(e.data)
+            file_end = buf.tell()
+
+            # seek to file table
+            buf.seek(0x1C, os.SEEK_SET)
+            # entry of this file
+            buf.seek(0xC*filename_to_index[e.filename], os.SEEK_CUR)
+            # first u32 is name offset
+            buf.seek(0x4, os.SEEK_CUR)
+            # write entry data
+            buf.write(struct.pack(f"{self.byte_order.struct} I", file_pos))
+            buf.write(struct.pack(f"{self.byte_order.struct} I", e.length))
+
+            # seek back
+            buf.seek(file_end, os.SEEK_SET)
+
+            i += 1
+
+        buf.seek(0, os.SEEK_END)
+        file_size = buf.tell()
+        buf.seek(0xC, os.SEEK_SET)
+        buf.write(self.byte_order.pack("I", file_size))
 
         return buf.getvalue()
 
@@ -122,7 +257,7 @@ class Darc:
 
         magic, bom = struct.unpack(Darc.IDENT_STRUCT, f.read(6))
         if magic != Darc.DARC_MAGIC:
-            raise TypeError(f"Invalid magic: expected {Darc.DARC_MAGIC} got {magic}")
+            raise TypeError(f"Invalid magic: expected {Darc.DARC_MAGIC!r} got {magic!r}")
 
         byte_order = ByteOrder.LITTLE_ENDIAN
         if bom == b"\xFF\xFE":
@@ -136,24 +271,30 @@ class Darc:
         if version != Darc.DARC_VERSION:
             raise TypeError(f"Unsupported version: expected {hex(Darc.DARC_VERSION)} got {hex(version)}")
 
-        print(hex(version))
-        print(f"file table offset is {hex(file_tab_off)}")
-        print(f"file table is {hex(file_tab_len)} bytes long ({file_tab_len // 12} entries)")
+        # print(hex(version))
+        # print(f"file table offset is {hex(file_tab_off)}")
+        # print(f"file table is {hex(file_tab_len)} bytes long ({file_tab_len // 12} entries)")
 
         # read root entry
-        _, _, num_entries = struct.unpack(Darc.FILE_TABLE_ENTRY.format(byte_order.struct), f.read(12))
+        _, _, end_index = struct.unpack(Darc.FILE_TABLE_ENTRY.format(byte_order.struct), f.read(12))
         root = DarcEntry("", is_dir=True)
 
         raw_file_table = []
-        for _ in range(num_entries - 1):
+        for _ in range(end_index - 1):
             raw_file_table.append(struct.unpack(Darc.FILE_TABLE_ENTRY.format(byte_order.struct), f.read(12)))
 
         name_table_start = f.tell()
-        print(f"name table offset is {hex(name_table_start)}")
+        # print(f"name table offset is {hex(name_table_start)}")
 
-        # dir, count
-        directory_stack = [[root, num_entries]]
-        for name_offset, file_offset, length in raw_file_table:
+        # dir, end
+        StackItem = namedtuple("StackItem", "node end")
+        directory_stack = [StackItem(root, end_index)]
+        testing_list = []
+        for i, (name_offset, file_offset, length) in enumerate(raw_file_table):
+            if directory_stack[-1].end == i:
+                # at end index
+                directory_stack.pop()
+
             is_dir = (name_offset & 0x01000000) != 0
             name_offset &= ~0x01000000
 
@@ -163,26 +304,27 @@ class Darc:
                 name += ch.decode(byte_order.wchar)
 
             entry = DarcEntry(name, is_dir)
-            directory_stack[-1][0].add_child(entry)
-            directory_stack[-1][1] -= 1
+            directory_stack[-1].node.add_child(entry)
 
-            # length is num entries for directory or file size for file
+            # length is end index for directory or file size for file
             if not is_dir:
                 # read file content
-                f.seek(file_dat_off + file_offset, os.SEEK_SET)
+                f.seek(file_offset, os.SEEK_SET)
                 entry.data = f.read(length)
+
+                testing_list.append((file_offset, entry.filename))
             else:
-                directory_stack.append([entry, length-3])  # why -3
+                directory_stack.append(StackItem(entry, length - 1))
 
-            if directory_stack[-1][1] == 0:
-                # no more entries left
-                directory_stack.pop()
+            # print(name)
+            # print(is_dir, name_offset, file_offset, length)
 
-            print(name)
-            print(is_dir, name_offset, file_offset, length)
+        # print(testing_list)
+        # print(root)
+        # root.dump()
 
-        print(root)
-        root.dump()
+        # for n in root.children():
+        #     print(n)
 
         arc = Darc(byte_order)
         arc.root_entry = root
@@ -195,8 +337,27 @@ def test() -> None:
         fb = f.read()
 
     arc = Darc.from_bytes(fb)
+
+    with open("/home/joseph/Documents/tomodachi_life/workspaces/scripts/test/test.bin", "wb") as f:
+        f.write(arc.to_bytes())
+    os.system("xxd /home/joseph/Documents/tomodachi_life/workspaces/scripts/test/test.bin > /home/joseph/Documents/tomodachi_life/workspaces/scripts/test/test.hex")
+
     print(flush=True)
-    assert arc.to_bytes() == fb
+
+    nb = arc.to_bytes()
+
+    assert len(nb) == len(fb)
+    print("lengths match")
+
+    assert nb == fb
+    print("files match")
+
+    assert nb == Darc.from_bytes(nb).to_bytes()
+    print("double indirection works")
+
+    print("IT WORKED??????")
+
+
 
 
 if __name__ == "__main__":
