f8121302cd63d0467243eba384750faea033ac19
Joseph Charamut <12399761+joecharamut@users.noreply.github.com>
2022-06-06 01:54:11 -0400

    some more block types

--- lib/lms.py
+++ lib/lms.py
@@ -3,13 +3,14 @@ import os
 import struct
 import io
 import typing
-from typing import Dict
+from typing import Dict, Type
 
 from lib.byteorder import ByteOrder, ByteOrderType
 
 
 # module for LibMessageStudio files
 # ref: https://github.com/kinnay/Nintendo-File-Formats/wiki/LMS-File-Format
+# ￼
 
 
 def read_char(f: typing.BinaryIO, encoding: str) -> str:
@@ -28,21 +29,21 @@ class LMSBlock(ABC):
         raise AssertionError
 
 
-class LMSUnknownBlock(LMSBlock):
+class UnknownBlock(LMSBlock):
     def __init__(self) -> None:
         self.data = bytes()
 
     @staticmethod
-    def from_bytes(data: bytes, lms_file: "LMSFile") -> "LMSUnknownBlock":
-        block = LMSUnknownBlock()
+    def from_bytes(data: bytes, lms_file: "LMSFile") -> "UnknownBlock":
+        block = UnknownBlock()
         block.data = data
         return block
 
     def to_bytes(self) -> bytes:
-        pass
+        return self.data
 
 
-class LMSHashTable(LMSBlock):
+class HashTableBlock(LMSBlock):
     def __init__(self, num_slots: int, byte_order: ByteOrderType, encoding: str) -> None:
         self.num_slots = num_slots
         self.labels = {}
@@ -57,7 +58,7 @@ class LMSHashTable(LMSBlock):
         return (val & 0xFFFFFFFF) % num_slots
 
     @staticmethod
-    def from_bytes(data: bytes, lms_file: "LMSFile") -> "LMSHashTable":
+    def from_bytes(data: bytes, lms_file: "LMSFile") -> "HashTableBlock":
         f = io.BytesIO(data)
         byte_order = lms_file.byte_order
         encoding = lms_file.encoding
@@ -80,7 +81,7 @@ class LMSHashTable(LMSBlock):
                     labels[label] = item
                 f.seek(pos, os.SEEK_SET)
 
-        tab = LMSHashTable(num_slots, byte_order, encoding)
+        tab = HashTableBlock(num_slots, byte_order, encoding)
         tab.labels = labels
         return tab
 
@@ -88,7 +89,71 @@ class LMSHashTable(LMSBlock):
         ...
 
     def __repr__(self) -> str:
-        return f"<LMSHashTable slots={self.num_slots} labels={self.labels!r}>"
+        return f"<HashTableBlock slots={self.num_slots} labels={self.labels!r}>"
+
+
+class CLR1Block(LMSBlock):
+    def __init__(self, byte_order: ByteOrderType = ByteOrder.LITTLE_ENDIAN) -> None:
+        self.colors = []
+        self.byte_order = byte_order
+
+    def __repr__(self) -> str:
+        return f"<CLR1Block colors={self.colors!r}>"
+
+    @staticmethod
+    def from_bytes(data: bytes, lms_file: "LMSFile") -> "CLR1Block":
+        f = io.BytesIO(data)
+
+        colors = []
+        num_colors, = lms_file.byte_order.unpack("I", f.read(4))
+        for _ in range(num_colors):
+            colors.append(lms_file.byte_order.unpack("BBBB", f.read(4)))
+
+        blk = CLR1Block()
+        blk.colors = colors
+        blk.byte_order = lms_file.byte_order
+        return blk
+
+    def to_bytes(self) -> bytes:
+        f = io.BytesIO()
+
+        f.write(self.byte_order.pack("I", len(self.colors)))
+        for c in self.colors:
+            f.write(self.byte_order.pack("BBBB", *c))
+
+        return f.getvalue()
+
+
+class ATI2Block(LMSBlock):
+    def __init__(self, byte_order: ByteOrderType = ByteOrder.LITTLE_ENDIAN) -> None:
+        self.attributes = []
+        self.byte_order = byte_order
+
+    def __repr__(self) -> str:
+        return f"<ATI2Block attributes={self.attributes!r}>"
+
+    @staticmethod
+    def from_bytes(data: bytes, lms_file: "LMSFile") -> "ATI2Block":
+        f = io.BytesIO(data)
+
+        attrs = []
+        num_attrs, = lms_file.byte_order.unpack("I", f.read(4))
+        for _ in range(num_attrs):
+            attrs.append(lms_file.byte_order.unpack("BBHI", f.read(8)))
+
+        blk = ATI2Block()
+        blk.attrs = attrs
+        blk.byte_order = lms_file.byte_order
+        return blk
+
+    def to_bytes(self) -> bytes:
+        f = io.BytesIO()
+
+        f.write(self.byte_order.pack("I", len(self.attributes)))
+        for a in self.attributes:
+            f.write(self.byte_order.pack("BBHI", *a))
+
+        return f.getvalue()
 
 
 class LMSFile:
@@ -144,8 +209,11 @@ class LMSFile:
         lms.blocks = blocks
         return lms
 
+    def to_bytes(self) -> bytes:
+        pass
 
-class Msbp:
+
+class LMSProjectFile:
     MSBP_MAGIC = b"MsgPrjBn"
 
     IDENT_STRUCT = "8s 2s"
@@ -157,33 +225,40 @@ class Msbp:
         ...
 
     @staticmethod
-    def from_bytes(data: bytes) -> "Msbp":
+    def from_bytes(data: bytes) -> "LMSProjectFile":
         lms = LMSFile.from_bytes(data)
 
         if lms.magic != Msbp.MSBP_MAGIC:
             raise TypeError(f"Invalid magic: expected {Msbp.MSBP_MAGIC!r} got {lms.magic!r}")
 
-        section_types = {
-            "CLR1": LMSUnknownBlock,
-            "CLB1": LMSHashTable,
-            "ATI2": LMSUnknownBlock,
-            "ALB1": LMSHashTable,
-            "ALI2": LMSUnknownBlock,
-            "TGG2": LMSUnknownBlock,
-            "TAG2": LMSUnknownBlock,
-            "TGP2": LMSUnknownBlock,
-            "TGL2": LMSUnknownBlock,
-            "SYL3": LMSUnknownBlock,
-            "SLB1": LMSHashTable,
-            "CTI1": LMSUnknownBlock,
+        section_types: Dict[str, Type[LMSBlock]] = {
+            "CLR1": CLR1Block,
+            "CLB1": HashTableBlock,
+            "ATI2": ATI2Block,
+            "ALB1": HashTableBlock,
+            "ALI2": UnknownBlock,
+            "TGG2": UnknownBlock,
+            "TAG2": UnknownBlock,
+            "TGP2": UnknownBlock,
+            "TGL2": UnknownBlock,
+            "SYL3": UnknownBlock,
+            "SLB1": HashTableBlock,
+            "CTI1": UnknownBlock,
         }
 
-        unpacked_sections = {}
+        unpacked_sections: Dict[str, LMSBlock] = {}
         for block_type, data in lms.blocks.items():
             if block_type not in section_types:
                 raise RuntimeError(f"Unhandled block type: {block_type}")
             unpacked_sections[block_type] = section_types[block_type].from_bytes(data, lms)
 
-        # clb1 = LMSHashTable.from_bytes(lms.blocks["CLB1"], lms)
+        # clb1 = HashTableBlock.from_bytes(lms.blocks["CLB1"], lms)
         # print(clb1)
-        print(unpacked_sections)
+
+        for k, v in unpacked_sections.items():
+            print(f"{k}: {v!r}")
+
+        assert unpacked_sections["CLR1"].to_bytes() == lms.blocks["CLR1"]
+
+
+Msbp = LMSProjectFile
